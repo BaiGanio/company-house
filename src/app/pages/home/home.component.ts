e00d4f128917747518ac76bbcc4457fdfea1cfdc
Lyuben Kikov <exceptionhelper@gmail.com>
2021-02-23 00:44:49 +0200

    Implemented dummy data fetching on button click.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from 'src/app/models/company.model';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  companies = new Array<Company>();
+  constructor(private backendService: BackendService) { }
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public getCompany(){
+    this.backendService.getCompany('alma').subscribe(response => {
+      response.forEach(element => {
+          const c = {
+            Id: element.id,
+            CompanyName: element.companyName,
+            CompanyNumber: element.companyNumber,
+            AddressLine1: element.addressLine1,
+            CompanyStatus: element.companyStatus,
+            CountryOfOrigin: element.countryOfOrigin
+          };
+          this.companies.push(c as Company);
+        });
+      }, 
+      error => {
+         console.log(error);
+      }, 
+      () => { 
+        console.log(this.companies); }
+    );
   }
-
 }

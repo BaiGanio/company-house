b0dc290b5e2405fafbf524e6ce4ae8dede67acc8
Lyuben Kikov <exceptionhelper@gmail.com>
2021-02-23 01:12:35 +0200

    set HttpHeaders

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Company } from '../models/company.model';
 @Injectable()
 export class BackendService {
 
   constructor(private http: HttpClient) { }
 
   getCompany(name: string): Observable<any> {
+    const headers: HttpHeaders = new HttpHeaders()
+    .set('Accept', 'application/json')
+    .set('Access-Control-Allow-Headers', 'Content-Type')
+    .set('Access-Control-Allow-Methods', 'GET')
+    .set('Access-Control-Allow-Origin', '*');
+
     return this.http.get(environment.bgapiUrl + name);
   }
 }
